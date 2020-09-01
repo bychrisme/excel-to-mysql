0233229b2647201c0acec0a8fa035bb9ce35f822
bychrisme <christianmeli81@gmail.com>
2020-09-01 18:15:30 +0100

    update size of file

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(bodyparser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(bodyparser.urlencoded({extended: true, limit: '50mb'}));
 app.use(fileUpload());
 
 app.get("/", (req, res) => {
